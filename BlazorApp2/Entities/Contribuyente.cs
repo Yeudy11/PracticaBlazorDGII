c5e4409d5fc01af2e8937e134f627fd8420a6b41
Smilt Yeudy <smiltyeudy@gmail.com>
2025-05-23 10:45:11 -0400

    Proyecto practica DGII Razor

---
+++ BlazorApp2/Entities/Contribuyente.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp2.Entities
+{
+    public class Contribuyente
+    {
+        [Required(ErrorMessage = "Nombre es obligatorio")]
+        public string Nombre { get; set; } = "";
+
+        [Required(ErrorMessage = "RncCedula obligatorio")]
+        public string RncCedula { get; set; } = "";
+
+        [Required(ErrorMessage = "Tipo es obligatorio")]
+        public string Tipo { get; set; } = "";
+
+        [Required(ErrorMessage = "Estatus es obligatorio")]
+        public string Estatus { get; set; } = "";
+    }
+}
---
+++ BlazorApp2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Security.Cryptography;
+using BlazorApp2.Components;
+using Microsoft.EntityFrameworkCore;
+using BlazorApp2.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// ✅ Obtener la cadena de conexión desde appsettings.json
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+#if DEBUG
+builder.Services.AddDbContextFactory<DBADGIIContext>(options =>
+    options.UseSqlServer(connectionString)
+           .EnableSensitiveDataLogging());
+#else
+builder.Services.AddDbContextFactory<DBADGIIContext>(options =>
+    options.UseSqlServer(connectionString));
+#endif
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+
+
+app.Run();
+
